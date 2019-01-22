5566b9148461be1d515cc9e668d42c34b79fc826
alexrbs <alexandrerbs1@gmail.com>
2019-01-22 03:01:08 +0000

    Update sales.php

--- sales.php
+++ sales.php
@@ -1,14 +1,120 @@
 <?PHP
-
   $authChk = true;
   require('app-lib.php');
+  isset($_POST['a'])? $action = $_POST['a'] : $action = "";
+  if(!$action) {
+    isset($_REQUEST['a'])? $action = $_REQUEST['a'] : $action = "";
+  }
+  isset($_POST['txtSearch'])? $txtSearch = $_POST['txtSearch'] : $txtSearch = "";
+  if(!$txtSearch) {
+    isset($_REQUEST['txtSearch'])? $txtSearch = $_REQUEST['txtSearch'] : $txtSearch = "";
+  }
   build_header($displayName);
 ?>
   <?PHP build_navBlock(); ?>
   <div id="content">
-    This is the <b><?PHP echo basename($_SERVER['PHP_SELF']); ?></b> page.
+    <div class="PageTitle">Sales and Invoicing Management Search</div>
+
+  <!-- Search Section Start -->
+    <form name="frmSearchInvoices" method="post"
+          id="frmSearchInvoices"
+          action="<?PHP echo $_SERVER['PHP_SELF']; ?>">
+      <div class="displayPane">
+        <div class="displayPaneCaption">Search:</div>
+        <div>
+          <input name="txtSearch" id="txtSearch" placeholder="Search Invoices"
+          style="width: calc(100% - 115px)" value="<?PHP echo $txtSearch; ?>">
+          <button type="button" id="btnSearch">Search</button>
+          <button type="button" id="btnAddRec">Add</button>
+        </div>
+      </div>
+      <input type="hidden" name="a" value="listInvoices">
+    </form>
+    <!-- Search Section End -->
+    <!-- Search Section List Start -->
+    <?PHP
+      if($action == "listInvoices") {
+    ?>
+    <div>
+      <table style="width: calc(100% - 15px);border: #cccccc solid 1px">
+        <tr style="background: #eeeeee">
+          <td style="width: 80px;border-left: #cccccc solid 1px"><b>Client Number</b></td>
+          <td style="border-left: #cccccc solid 1px"><b>Client Name</b></td>
+          <td style="width: 80px;text-align: right"><b>Price</b></td>
+        </tr>
+    <?PHP
+      openDB();
+      $query =
+        "SELECT
+            *
+         FROM
+            lpa_invoices
+         WHERE
+            lpa_inv_client_ID LIKE '%$txtSearch%'
+         OR
+            lpa_inv_client_name LIKE '%$txtSearch%'
+
+         ";
+      $result = $db->query($query);
+      $row_cnt = $result->num_rows;
+      if($row_cnt >= 1) {
+        while ($row = $result->fetch_assoc()) {
+          $sid = $row['lpa_inv_no'];
+          ?>
+          <tr class="hl">
+            <td onclick="loadClientName(<?PHP echo $sid; ?>,'Edit')"
+                style="cursor: pointer;border-left: #cccccc solid 1px">
+              <?PHP echo $sid; ?>
+            </td>
+            <td onclick="loadClientName(<?PHP echo $sid; ?>,'Edit')"
+                style="cursor: pointer;border-left: #cccccc solid 1px">
+                <?PHP echo $row['lpa_inv_client_name']; ?>
+            </td>
+            <td style="text-align: right">
+              <?PHP echo $row['lpa_inv_amount']; ?>
+            </td>
+          </tr>
+        <?PHP }
+      } else { ?>
+        <tr>
+          <td colspan="3" style="text-align: center">
+            No Records Found for: <b><?PHP echo $txtSearch; ?></b>
+          </td>
+        </tr>
+      <?PHP } ?>
+      </table>
+    </div>
+    <?PHP } ?>
+    <!-- Search Section List End -->
   </div>
   <script>
+    var action = "<?PHP echo $action; ?>";
+    var search = "<?PHP echo $txtSearch; ?>";
+    if(action == "recUpdate") {
+      alert("Record Updated!");
+      navMan("sales.php?a=listInvoices&txtSearch=" + search);
+    }
+    if(action == "recInsert") {
+      alert("Record Added!");
+      navMan("sales.php?a=listInvoices&txtSearch=" + search);
+    }
+    if(action == "recDel") {
+      alert("Record Deleted!");
+      navMan("sales.php?a=listInvoices&txtSearch=" + search);
+    }
+    function loadClientName(ID,MODE) {
+      window.location = "stockaddedit.php?sid=" +  //Need to create the invoice edit file
+      ID + "&a=" + MODE + "&txtSearch=" + search;
+    }
+    $("#btnSearch").click(function() {
+      $("#frmSearchInvoices").submit();
+    });
+    $("#btnAddRec").click(function() {
+      loadClientName("","Add");
+    });
+    setTimeout(function(){
+      $("#txtSearch").select().focus();
+    },1);
   </script>
 <?PHP
 build_footer();
