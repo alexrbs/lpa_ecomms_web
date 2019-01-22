177e8428cc5a4fa1bc8ee1db78d6f254b1273764
alexrbs <alexandrerbs1@gmail.com>
2019-01-22 01:38:42 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <div id="footer">
-      <div id="copyright">Copyright &copy; &lt;YOUR NAME&gt; <?PHP echo date("Y"); ?></div>
+      <div id="copyright">Copyright &copy; &lt;Alexandre Ribas&gt; <?PHP echo date("Y"); ?></div>
     </div>
   </body>
 </html>  
