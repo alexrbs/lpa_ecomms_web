c036bb9ce838dd4facde922601f4c3654f58693a
Alexandre Ribas <alexandrerbs1@gmail.com>
2019-01-26 14:39:33 +1000

    Update header.php

--- header.php
+++ header.php
@@ -15,6 +15,6 @@
       </div>
     </div>
     <div style="text-align:right">Welcome <?php echo $displayName; ?>
-        <br> Status: <?php echo $displayGroup ?> //added to display user privileges
+        <br> Status: <?php echo $displayGroup ?> <!--added to display user privileges-->
     </div>
     <div style="clear: right"></div>
