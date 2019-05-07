cd1b8001588560c5d92f382e0acebbbd3c3b2072
alexander <1081475+alexrbs@users.noreply.github.com>
2019-05-07 13:43:31 +1000

    Index changes. Mashups for maps, youtube and facebook.

--- index.php
+++ index.php
@@ -6,7 +6,31 @@
 ?>
   <?PHP build_navBlock(); ?>
   <div id="content">
-    This is the <b><?PHP echo basename($_SERVER['PHP_SELF']); ?></b> page.
+    <div id="mission">
+      <b>Our Mission:</b><br>
+      <p>
+        Logical Peripherals Australia’s mission is to supply high quality computer peripherals,
+        reliable customer service and above all customer satisfaction. We strive to deliver the
+        very best in the latest technologies and support our customers with the highest after
+        sales support, allowing our customers to enjoy the best of technology that our
+        ever-changing world demands.
+      </p>
+      </div>
+      <div id="maps">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d885.1829090490334!2d153.18495718814026!3d-27.446473098926035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b915fc934af82f5%3A0x9c7d2b88ab23bc25!2s61+Lower+Esplanade%2C+Manly+QLD+4179!5e0!3m2!1sen!2sau!4v1557195615490!5m2!1sen!2sau"
+        width="440" height="315" frameborder="0" style="border:0" allowfullscreen>
+        </iframe>
+      </div>
+      <div id="youtube">
+        <iframe width="440" height="315" src="https://www.youtube.com/embed/cnHWT-7HO8o"
+        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
+      <div id="facebook">
+        <iframe src="https://www.facebook.com/fcinformaticars/"
+        width="440" height="315" frameborder="0" style="border:0" allowfullscreen>
+        </iframe>
+      </div>
   </div>
   <script>
   </script>
